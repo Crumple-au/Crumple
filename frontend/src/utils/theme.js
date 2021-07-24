67931f476550fdd4389e4196503a53216442bcef
irah rosete <irah.rosete@outlook.com>
2021-07-24 17:35:42 +1000

    setup theme

---
+++ frontend/src/utils/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@material-ui/core'
+
+export const theme = createTheme({
+  palette: {
+    common: {
+      black: '#323232',
+    },
+    primary: {
+      main: '#6184C4',
+    },
+    secondary: {
+      main: '#F62D01',
+    },
+  },
+  typography: {
+    fontFamily: 'Prompt',
+  },
+})
