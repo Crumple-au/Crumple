a58aab7d0143e067dbfc3c25ee3ef4758d449c3d
irah rosete <irah.rosete@outlook.com>
2021-07-25 14:00:38 +1000

    update routes and links and add error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import { ThemeProvider } from '@material-ui/core'
 
 import HomePage from './screens/HomePage'
 import NavBar from './components/NavBar'
+import Categories from './components/Categories.jsx'
+import About from './screens/About.jsx'
+import FAQs from './screens/FAQs.jsx'
+import Error from './screens/Error.jsx'
 import { theme } from './utils/theme'
 
 const App = () => {
@@ -14,6 +18,18 @@ const App = () => {
         <Switch>
           <Route exact path='/'>
             <HomePage />
+          </Route>
+          <Route path='/discover'>
+            <Categories />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/faqs'>
+            <FAQs />
+          </Route>
+          <Route path='*'>
+            <Error />
           </Route>
         </Switch>
       </Router>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Box, Typography, AppBar, Toolbar, Grid } from '@material-ui/core'
+
 import { black } from '../utils/theme'
 import logo from '../images/crumple-logo.jpg'
 
@@ -8,16 +10,18 @@ const NavBar = () => {
     <AppBar style={{ backgroundColor: black }} position='sticky'>
       <Toolbar>
         <Box pl='5rem' py='0.5rem'>
-          <img src={logo} alt='logo' width='60' />
+          <Link to='/'>
+            <img src={logo} alt='logo' width='60' />
+          </Link>
         </Box>
         <Box pr='5rem'>
           <Typography variant='h5'>crumple</Typography>
         </Box>
         <Box p='1.5rem'>
-          <Typography>DISCOVER</Typography>
+          <Link to='/discover'>DISCOVER</Link>
         </Box>
         <Box p='1.5rem'>
-          <Typography>ABOUT</Typography>
+          <Link to='/about'>ABOUT</Link>
         </Box>
         <Grid container justifyContent='flex-end'>
           <Box p='1.5rem'>
