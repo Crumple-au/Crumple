4f89ad66cbb3134c931043d956741f4fa31cefec
irah rosete <irah.rosete@outlook.com>
2021-07-25 19:35:05 +1000

    add stickman on landing page

--- frontend/src/screens/HomePage.jsx
+++ frontend/src/screens/HomePage.jsx
@@ -1,28 +1,47 @@
 import React from 'react'
-import { Typography, Box } from '@material-ui/core'
+import { Link as RouterLink } from 'react-router-dom'
+import { Typography, Box, Button } from '@material-ui/core'
+
 import { primary, secondary } from '../utils/theme'
+import stick0 from '../images/crumple-stick0.jpg'
 
 const HomePage = () => {
   return (
-    <Box
-      display='flex'
-      flexDirection='column'
-      height='65vh'
-      width='auto'
-      alignItems='center'
-      justifyContent='center'
-    >
-      <Typography variant='h1'>made for makers</Typography>
-      <Typography variant='h3' color='textSecondary'>
-        <span style={{ color: primary }}>crumple</span> makes it easy
-      </Typography>
-      <Typography variant='h3' color='textSecondary'>
-        for <span style={{ color: secondary }}>artists</span> and{' '}
-        <span style={{ color: secondary }}>creatives</span>
-      </Typography>
-      <Typography variant='h3' color='textSecondary'>
-        to get paid for their work.
-      </Typography>
+    <Box my='4vh'>
+      <Box
+        display='flex'
+        flexDirection='column'
+        height='30vh'
+        width='auto'
+        alignItems='center'
+        justifyContent='center'
+        pt='11vh'
+      >
+        <Typography variant='h1'>made for makers</Typography>
+        <Typography variant='h4' color='textSecondary'>
+          <span style={{ color: primary }}>crumple</span> makes it easy
+        </Typography>
+        <Typography variant='h4' color='textSecondary'>
+          for <span style={{ color: secondary }}>artists</span> and{' '}
+          <span style={{ color: secondary }}>creatives</span>
+        </Typography>
+        <Typography variant='h4' color='textSecondary'>
+          to get paid for their work.
+        </Typography>
+        <Box m='2rem'>
+          <Button
+            component={RouterLink}
+            to='/discover'
+            variant='contained'
+            color='primary'
+          >
+            Get Started
+          </Button>
+        </Box>
+      </Box>
+      <Box p='3rem' pl='5rem'>
+        <img src={stick0} alt='stickman' height='900' width='auto'></img>
+      </Box>
     </Box>
   )
 }
