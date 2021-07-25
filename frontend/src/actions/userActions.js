1199ca9813738023c56bdc19556a72b7761984d7
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-07-25 17:32:55 +1000

    updated frontend for deployment

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,7 +4,7 @@ import {USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL} from "../con
 const signin = (email, password) => async (dispatch) => {
     dispatch({ type: USER_SIGNIN_REQUEST, payload: { email, password } });
     try {
-        const { data } = await Axios.post("/api/users/signin", { email, password });
+        const { data } = await Axios.post(process.env.PRODUCTION_URL + "/api/users/signin", { email, password });
         dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
     } catch (error) {
         dispatch({ type: USER_SIGNIN_FAIL, payload: error.message });
