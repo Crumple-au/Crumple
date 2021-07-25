206ae34d6b3c46464a1a53bc13a25597bb5af09f
irah rosete <irah.rosete@outlook.com>
2021-07-25 15:12:13 +1000

    style navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaShoppingCart } from 'react-icons/fa'
 import { Box, Typography, AppBar, Toolbar, Grid } from '@material-ui/core'
 
-import { black } from '../utils/theme'
+import { black, useStyles } from '../utils/theme'
 import logo from '../images/crumple-logo.jpg'
 
 const NavBar = () => {
+  const classes = useStyles()
+
   return (
     <AppBar style={{ backgroundColor: black }} position='sticky'>
       <Toolbar>
@@ -15,17 +18,23 @@ const NavBar = () => {
           </Link>
         </Box>
         <Box pr='5rem'>
-          <Typography variant='h5'>crumple</Typography>
+          <Link to='/' className={classes.navlink}>
+            <Typography variant='h5'>crumple</Typography>
+          </Link>
         </Box>
         <Box p='1.5rem'>
-          <Link to='/discover'>DISCOVER</Link>
+          <Link to='/discover' className={classes.navlink}>
+            DISCOVER
+          </Link>
         </Box>
         <Box p='1.5rem'>
-          <Link to='/about'>ABOUT</Link>
+          <Link to='/about' className={classes.navlink}>
+            ABOUT
+          </Link>
         </Box>
         <Grid container justifyContent='flex-end'>
           <Box p='1.5rem'>
-            <Typography>CART</Typography>
+            <FaShoppingCart />
           </Box>
           <Box p='1.5rem'>
             <Typography>LOGIN</Typography>
