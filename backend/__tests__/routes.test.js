3245c63ab00651f1d6f3f16424dd6e88c4a16825
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-07-27 16:57:14 +1000

    C:/Program Files/Git/signin test passed

--- backend/__tests__/routes.test.js
+++ backend/__tests__/routes.test.js
@@ -1,5 +1,3 @@
-jest.useFakeTimers()
-
 // import app from '../testServer.js'
 import request  from 'supertest';
 import express from 'express'
@@ -7,43 +5,60 @@ import mongoose from 'mongoose';
 import seed from '../util/seed.js';
 import User from '../models/userModel.js';
 import userRouter from '../routes/userRouter';
+import {generateToken} from '../util/util.js';
+import { Db } from 'mongodb';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/users', userRouter);
-app.get('/user', function(req, res) {
-    res.status(200).json({ name: 'john' });
-});
+// mongoose.connect('mongodb://localhost/crumple' , {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+// });
+
+// app.use('/api/users', userRouter);
 
 app.get('/seed', function(req, res) {
     const createdUsers = User.insertMany(seed.users);
     res.status(200).json({ createdUsers })
+});
+
+app.post('/signin', async(req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.send(400)
+        return
+    }
+
+    res.status(200).json({email})
 })
+
+
+
 describe('User Endpoints', () => {
     it('Should get data from /seed', (done) => {
+        jest.useFakeTimers()
+
         request(app)
             .get('/seed')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200, done);
+    });
+
+    it("should respond with a 200 status code", async() => {
+        jest.runAllTimers()
+
+        const response = await request(app).post("/signin").send({ 
+            email: "user2@example.com", 
+            password: '1234' 
+        })
+        console.log(response.statusCode);
+        expect(response.statusCode).toBe(200);
     })
-
-    // it('should post to /signin', async () => {
-    //     const response = await request(app)
-    //         .post('/signin')
-    //         .send({
-    //             email: 'admin1@example.com',
-    //             password: 1234
-    //         })
-    //         console.log(response)
-    //     expect(response.body).toHaveProperty('user');
-
-    // });
-
-
 
 });
 
