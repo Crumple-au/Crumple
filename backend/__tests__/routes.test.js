21625655311bd4ec511b1ae17d9528232df3d237
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-07-27 15:31:33 +1000

    passed /seed test

--- backend/__tests__/routes.test.js
+++ backend/__tests__/routes.test.js
@@ -1,14 +1,34 @@
 jest.useFakeTimers()
 
-import app from '../testServer.js'
+// import app from '../testServer.js'
 import request  from 'supertest';
+import express from 'express'
+import mongoose from 'mongoose';
+import seed from '../util/seed.js';
+import User from '../models/userModel.js';
+import userRouter from '../routes/userRouter';
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRouter);
+app.get('/user', function(req, res) {
+    res.status(200).json({ name: 'john' });
+});
+
+app.get('/seed', function(req, res) {
+    const createdUsers = User.insertMany(seed.users);
+    res.status(200).json({ createdUsers })
+})
 describe('User Endpoints', () => {
-    it('Should get data from /seed', async () => {
-        const response = await request(app).get('/seed');
-        console.log(response.body)
-        expect(response.statusCode).toEqual(200);
-        // expect(response.body).toHaveProperty('createdUsers');
+    it('Should get data from /seed', (done) => {
+        request(app)
+            .get('/seed')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     })
 
     // it('should post to /signin', async () => {
