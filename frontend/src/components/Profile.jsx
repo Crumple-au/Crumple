57fc41e950ea6953ee5ed1f60ab1b11193913828
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-07-29 15:52:57 +1000

    added routes in Profile page

---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Profile({ user }) {
+    return (
+        <main className="main">
+            {user && (
+                <>
+                    <h1>{user.name }'s Profile page</h1>
+                    <h1>{user.email }</h1>
+                    <h1>Admin: {String(user.isAdmin) }</h1>
+                    <h1>Seller: {String(user.isSeller) }</h1>     
+                    <p>{user.description}</p> 
+                </>
+            )}
+        </main>
+    )
+}
+
+export default Profile
