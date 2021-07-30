b4a568db5c1d430ef8333e506efb0c1a421494dd
irah rosete <irah.rosete@outlook.com>
2021-07-31 04:23:08 +1000

    build for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,54 @@
 import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
 // const cors = require('cors')
 import userRouter from './routes/userRouter.js'
-import path from 'path';
+import path from 'path'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 const corsOptions = {
-    origin: 'https://crumple.netlify.app',
-    optionsSuccessStatus: 200 // For legacy browser support
+  origin: 'https://crumple.netlify.app',
+  optionsSuccessStatus: 200, // For legacy browser support
 }
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "https://crumple.netlify.app"); // update to match the domain you will make the request from
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 // });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/crumple', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-// app.get('/', (req, res) => { 
+// app.get('/', (req, res) => {
 //     res.send('Hello from Express!')
 // })
 
-app.use('/api/users', userRouter);
+app.use('/api/users', userRouter)
 
-app.use(express.static(__dirname + "/frontend/build"))
+app.use(express.static(__dirname + '/frontend/build'))
 
-if (process.env.NODE_ENV === 'production') {  
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-    app.get("/*", (_, res) => {
-        res.sendFile(path.join(__dirname, "/frontend/build/", "index.html"));
-    });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('/*', (_, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build/', 'index.html'))
+  })
 }
 
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
