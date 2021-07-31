229b2d606d21ef00bc7929d52a72db5b6c4034c1
irah rosete <irah.rosete@outlook.com>
2021-07-31 14:21:55 +1000

    update categories and make it responsive

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from './SearchBar'
 import {
   Box,
+  Grid,
   Typography,
   Card,
   CardActionArea,
@@ -10,43 +11,50 @@ import {
   CardActions,
   Button,
 } from '@material-ui/core'
-import { FaPaintBrush } from 'react-icons/fa'
 
 import { useStyles } from '../utils/theme'
+import { data } from '../data/categories'
 
 const Categories = () => {
   const classes = useStyles()
+  const [categories, setCategories] = useState(data)
 
   return (
     <>
       <SearchBar />
-      <Box m='2rem'>
-        <Card className={classes.card}>
-          <CardActionArea>
-            <CardMedia>
-              <Box display='flex' justifyContent='center' p='1rem' border={3}>
-                <FaPaintBrush fontSize='10rem' />
-              </Box>
-            </CardMedia>
-            <CardContent>
-              <Typography align='center'>Drawing & Painting</Typography>
-              <Typography align='center' color='textSecondary'>
-                Art finds
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size='small'>Explore</Button>
-          </CardActions>
-        </Card>
 
-        <Typography>Design & Tech</Typography>
-        <Typography>Merchandise</Typography>
-        <Typography>Books & Writing</Typography>
-        <Typography>Photography</Typography>
-        <Typography>Film & Video</Typography>
-        <Typography>Music & Sound Design</Typography>
-        <Typography>Crafts & DIY</Typography>
+      <Box m='2rem'>
+        <Grid container spacing={0} justifyContent='center'>
+          {categories.map((category) => {
+            return (
+              <Grid item xs={12} sm={6} md={3} align='center' key={category.id}>
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia>
+                      <Box
+                        display='flex'
+                        justifyContent='center'
+                        p='1rem'
+                        border={3}
+                      >
+                        {category.image}
+                      </Box>
+                    </CardMedia>
+                    <CardContent>
+                      <Typography align='center'>{category.name}</Typography>
+                      <Typography align='center' color='textSecondary'>
+                        {category.description}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Button size='small'>Explore</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            )
+          })}
+        </Grid>
       </Box>
     </>
   )
