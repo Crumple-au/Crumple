8afc7163bf5b454812d1e33096e7416295b80cb8
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-08-01 13:13:32 +1000

    order model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+    {
+        orderItems: [
+            {
+                name: { type: String, required: true },
+                qty: { type: Number, required: true },
+                price: { type: Number, required: true },
+                artwork: { type: mongoose.Schema.Types.ObjectId, ref: 'Artwork', required: true },
+            },
+        ],
+        shippingAddress: {
+            fullName: { type: String, required: true },
+            address: { type: String, required: true },
+            city: { type: String, required: true },
+            postalCode: { type: String, required: true },
+            country: { type: String, required: true },
+        },
+        paymentMethod: { type: String, required: true },
+        itemsPrice: { type: Number, required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+        isPaid: { type: Boolean, default: false },
+        paidAt: { type: Date },
+    },
+    {
+        timestamps: true,
+    }
+);
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
