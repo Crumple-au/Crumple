4f5f1e9514855efd153f5351837feae566c449a0
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-08-03 15:18:06 +1000

    added styling to StepsUI componenent

--- frontend/src/components/StepsUI.jsx
+++ frontend/src/components/StepsUI.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function StepsUI(props) {
     console.log(props)
     return (
-        <div>
+        <div className="steps-ui">
             <div className={props.step1 ? 'active' : ''}>{props.text.signin}</div>
             <div className={props.step2 ? 'active' : ''}>{props.text.shipping}</div>
             <div className={props.step3 ? 'active' : ''}>{props.text.payment}</div>
