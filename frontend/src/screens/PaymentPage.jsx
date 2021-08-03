6d8a4fd5a142cd80e5face264286700afc8ee330
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-08-03 15:50:56 +1000

    added payment form

--- frontend/src/screens/PaymentPage.jsx
+++ frontend/src/screens/PaymentPage.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import StepsUI from '../components/StepsUI';
+
+const shippingSteps = {
+    text1: 'Signin',
+    text2: 'Shipping',
+    text3: 'Payment',
+    text4: 'Place Order'
+}
 
 function PaymentPage() {
     return (
         <div>
-            payment page
+            <StepsUI step1 step2 step3 text={shippingSteps}></StepsUI>
+
+            <div>
+                <h3>Payment Method</h3>
+            </div>
+
+            <form>
+                <div>
+                    <input
+                    type="radio"
+                    id="paypal"
+                    value="paypal"
+                    name="paymentMethod"
+                    required
+                    checked
+                    >
+                    </input>
+                    <label htmlFor="paypal">Paypal</label>
+                </div>
+                <div>
+                    <input
+                    type="radio"
+                    id="stripe"
+                    value="stripe"
+                    name="paymentMethod"
+                    required
+                    >
+                    </input>
+                    <label htmlFor="stripe">Stripe</label>
+                </div>
+                <button className="primary" type="submit">
+                    Continue
+                </button>
+            </form>
         </div>
     )
 }
