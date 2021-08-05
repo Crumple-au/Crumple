805d36572416f2c34aeeb0bed35e91ca638a2b5b
irah rosete <irah.rosete@outlook.com>
2021-08-05 22:42:29 +1000

    add artworks in category and style

--- frontend/src/components/Artworks.jsx
+++ frontend/src/components/Artworks.jsx
@@ -1,18 +1,81 @@
 import React from 'react'
+import {
+  Box,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Link,
+  Button,
+} from '@material-ui/core'
+import { useStyles } from '../utils/theme'
+import art from '../images/crumple-artwork.jpg'
 
-const Artworks = ({user, artworks}) => {
-  return <main className="main">
+const Artworks = ({ user, artworks }) => {
+  const classes = useStyles()
+
+  console.log(artworks)
+  return (
+    <>
+      <Box m='4rem 2rem 2rem 2rem'>
+        <Grid container spacing={1} justifyContent='center' alignItems='center'>
+          {artworks.map((artwork) => {
+            return (
+              <Grid item xs={12} sm={6} md={3} align='center' key={artwork._id}>
+                <Card className={classes.card}>
+                  <CardActionArea>
+                    <CardMedia>
+                      <Box
+                        display='flex'
+                        justifyContent='center'
+                        p='1rem'
+                        border={3}
+                      >
+                        <img
+                          src={art}
+                          alt={artwork.name}
+                          height='100px'
+                          width='100px'
+                        />
+                      </Box>
+                    </CardMedia>
+                    <CardContent>
+                      <Typography align='center'>{artwork.name}</Typography>
+                      <Typography align='center' color='textSecondary'>
+                        By x artist name
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+
+                  <CardActions>
+                    <Link to='#'>
+                      <Button size='small'>${artwork.price}</Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </Box>
+
+      {/* <main className='main'>
       {user.name}'s Artworks
-        {artworks.map((artwork) => (
-          <ul key={artwork._id}>
-            <li>{artwork.name}</li>
-            <li>Category: {artwork.category}</li>
-            <li>{artwork.description}</li>
-            <li>${artwork.price}</li>
-            <br></br>
-          </ul>
-        ))}
-    </main>
+      {artworks.map((artwork) => (
+        <ul key={artwork._id}>
+          <li>{artwork.name}</li>
+          <li>Category: {artwork.category}</li>
+          <li>{artwork.description}</li>
+          <li>${artwork.price}</li>
+          <br></br>
+        </ul>
+      ))}
+    </main> */}
+    </>
+  )
 }
 
 export default Artworks
