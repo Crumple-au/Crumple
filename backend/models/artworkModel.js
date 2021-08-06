1ca00bd2c06f150b640d67275384dfef1778b2e8
irah rosete <irah.rosete@outlook.com>
2021-08-06 14:20:55 +1000

    update seeds

--- backend/models/artworkModel.js
+++ backend/models/artworkModel.js
@@ -1,18 +1,18 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const artworkSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true},
-        image: {type: String },
-        description: { type: String, maxlength: 250, trim: true },
-        price: { type: Number, required: true },
-        inStock: { type: Number, required: true },
-        seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
-        category: { type: String, ref: 'Category'  }
-    },
-    {
-        timestamps: true,
-    }
-);
-const Artwork = mongoose.model('Artwork', artworkSchema);
-export default Artwork;
+  {
+    name: { type: String, required: true },
+    image: { type: String },
+    description: { type: String, maxlength: 250, trim: true },
+    price: { type: Number, required: true },
+    inStock: { type: Number, required: true },
+    seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+    category: { type: String, ref: 'Category', required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+const Artwork = mongoose.model('Artwork', artworkSchema)
+export default Artwork
