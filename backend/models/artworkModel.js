a5a1c739a5b22b9028c77d61e1645380c088c514
[Ryan Tavcar] <[Ryan.Tavcar@gmail.com]>
2021-08-07 15:50:05 +1000

    changed artwork and user description length

--- backend/models/artworkModel.js
+++ backend/models/artworkModel.js
@@ -4,7 +4,7 @@ const artworkSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     image: { type: String },
-    description: { type: String, maxlength: 250, trim: true },
+    description: { type: String, maxlength: 500, trim: true },
     price: { type: Number, required: true },
     inStock: { type: Number, required: true },
     seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
