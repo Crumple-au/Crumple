f4a98fb4b2110d0dabd70cd1cc78388b89afaf71
irah rosete <irah.rosete@outlook.com>
2021-08-08 20:24:44 +1000

    style profile artworks

--- frontend/src/screens/ProfilePage.jsx
+++ frontend/src/screens/ProfilePage.jsx
@@ -17,12 +17,11 @@ import EditProfilePage from '../components/EditProfilePage'
 import Alert from '../components/Alert'
 import Preloader from '../components/Preloader'
 import ProfileArtworksPage from './ProfileArtworksPage'
-import Chip from '@material-ui/core/Chip';
-import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined';
-import PaymentOutlinedIcon from '@material-ui/icons/PaymentOutlined';
-import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
-import LiveHelpOutlinedIcon from '@material-ui/icons/LiveHelpOutlined';
-
+import Chip from '@material-ui/core/Chip'
+import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined'
+import PaymentOutlinedIcon from '@material-ui/icons/PaymentOutlined'
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined'
+import LiveHelpOutlinedIcon from '@material-ui/icons/LiveHelpOutlined'
 
 function ProfilePage() {
   const { userId } = useParams()
@@ -33,95 +32,124 @@ function ProfilePage() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    if (!user ) {
+    if (!user) {
       dispatch(detailsUser(userId))
     }
   }, [dispatch, user, userId])
 
   return (
     <BrowserRouter>
-    <div className="profile-container">
-      <aside className="sidebar">
+      <div className='profile-container'>
+        <aside className='sidebar'>
+          <div className='heading'>
+            {user ? (
+              <>
+                <img src={user.image} alt='profile'></img>
+                <div className='user-name'>
+                  <h1>{user.name}</h1>
+                  {user.isAdmin ? (
+                    <Chip
+                      label='admin'
+                      color='primary'
+                      className='admin-badge'
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </>
+            ) : (
+              <Preloader />
+            )}
+          </div>
+          <div className='description'>
+            <p>{user && user.description}</p>
+          </div>
 
-        <div className="heading">
-          {user ?
-            <>
-              <img  src={user.image} alt="profile"></img>
-              <div className="user-name">
-                <h1>{user.name}</h1>
-                {user.isAdmin ? 
-                    <Chip 
-                        label="admin"
-                        color="primary"
-                        className="admin-badge"
-                    /> 
-                    : ''}
-              </div>
-            </>  
-          : <Preloader/>}  
-        </div>
-        <div className="description">
-          <p>{user && user.description}</p>
-        </div>
-        
-        <div className="sidebar-links">
-          <div className="ul">
-            <div className="li">
+          <div className='sidebar-links'>
+            <div className='ul'>
+              <div className='li'>
                 {/* <CameraAltOutlinedIcon style={{color: 'ivory'}} /> */}
-                <NavLink className="link" exact activeClassName="selected" to={`/profile/${userId}/`} >
-                <span><CameraAltOutlinedIcon style={{color: 'ivory'}} /></span>Artworks
-              </NavLink>
-            </div>
-            <div className="li">
+                <NavLink
+                  className='link'
+                  exact
+                  activeClassName='selected'
+                  to={`/profile/${userId}/`}
+                >
+                  <span>
+                    <CameraAltOutlinedIcon style={{ color: 'ivory' }} />
+                  </span>
+                  Artworks
+                </NavLink>
+              </div>
+              <div className='li'>
                 {/* <PaymentOutlinedIcon style={{color: 'ivory'}}/> */}
-              <NavLink className="link" activeClassName="selected"  to={`/profile/${userId}/payments`} >
-              <span><PaymentOutlinedIcon style={{color: 'ivory'}}/></span>Payments
-              </NavLink>
+                <NavLink
+                  className='link'
+                  activeClassName='selected'
+                  to={`/profile/${userId}/payments`}
+                >
+                  <span>
+                    <PaymentOutlinedIcon style={{ color: 'ivory' }} />
+                  </span>
+                  Payments
+                </NavLink>
+              </div>
+              <div className='li'>
+                {/* <EditOutlinedIcon style={{color: 'ivory'}}/> */}
+                <NavLink
+                  className='link'
+                  activeClassName='selected'
+                  to={`/profile/${userId}/settings`}
+                >
+                  <span>
+                    <EditOutlinedIcon style={{ color: 'ivory' }} />
+                  </span>
+                  Edit
+                </NavLink>
+              </div>
+              <div className='li'>
+                {/* <LiveHelpOutlinedIcon style={{color: 'ivory'}}/> */}
+                <NavLink
+                  className='link'
+                  activeClassName='selected'
+                  to={`/profile/${userId}/faq`}
+                >
+                  <span>
+                    <LiveHelpOutlinedIcon style={{ color: 'ivory' }} />
+                  </span>
+                  FAQ
+                </NavLink>
+              </div>
             </div>
-            <div className="li">
-              {/* <EditOutlinedIcon style={{color: 'ivory'}}/> */}
-              <NavLink className="link" activeClassName="selected" to={`/profile/${userId}/settings`} >
-              <span><EditOutlinedIcon style={{color: 'ivory'}}/></span>Edit
-              </NavLink>
-            </div>
-            <div className="li">
-              {/* <LiveHelpOutlinedIcon style={{color: 'ivory'}}/> */}
-              <NavLink className="link" activeClassName="selected"  to={`/profile/${userId}/faq`} >
-              <span><LiveHelpOutlinedIcon style={{color: 'ivory'}}/></span>FAQ
-              </NavLink>
+
+            <div className='sidebar-footer'>
+              <p>Having troubles?</p>
+              <p>Contact us</p>
             </div>
           </div>
-          
-          <div className="sidebar-footer">
-            <p>Having troubles?</p>
-            <p>Contact us</p>
-          </div>
+        </aside>
+
+        <div>
+          {loading ? (
+            <Preloader />
+          ) : error ? (
+            <Alert variant='danger'>{error}</Alert>
+          ) : (
+            <Switch>
+              <Route path='/profile/:userId/payments'>
+                <Payments />
+              </Route>
+              <Route path='/profile/:userId/settings'>
+                <EditProfilePage />
+              </Route>
+              <Route path='/profile/:userId/'>
+                <ProfileArtworksPage />
+              </Route>
+            </Switch>
+          )}
         </div>
-      </aside>
-    
-      <main>
-        {loading ? (
-          <Preloader/>
-        ) : error ? (
-          <Alert variant="danger">{error}</Alert>
-        ) : (
-
-          <Switch>
-            <Route path='/profile/:userId/payments'>
-              <Payments />
-            </Route>
-            <Route path='/profile/:userId/settings'>
-              <EditProfilePage />
-            </Route>
-            <Route path='/profile/:userId/'>
-              <ProfileArtworksPage />
-            </Route>
-          </Switch>
-        )}
-      </main>
-      
-
-    </div>
+      </div>
     </BrowserRouter>
   )
 }
