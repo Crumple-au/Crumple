5751ff82ba436e669f59548c9b20cc687a1952f3
irah rosete <irah.rosete@outlook.com>
2021-08-11 10:55:12 +1000

    style shipping page and make responsive

--- frontend/src/screens/ShippingPage.jsx
+++ frontend/src/screens/ShippingPage.jsx
@@ -1,121 +1,198 @@
-import React, {useState} from 'react'
-import '../style/order.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { saveShippingAddress } from '../actions/cartActions';
-import StepsUI from '../components/StepsUI';
+import React, { useState } from 'react'
+import '../style/order.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { saveShippingAddress } from '../actions/cartActions'
+import StepsUI from '../components/StepsUI'
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  InputLabel,
+  OutlinedInput,
+} from '@material-ui/core'
+
+import { secondary } from '../utils/theme'
 
 const shippingSteps = {
-    text1: 'Signin',
-    text2: 'Shipping',
-    text3: 'Payment',
-    text4: 'Place Order'
+  text1: 'Sign In',
+  text2: 'Shipping',
+  text3: 'Payment',
+  text4: 'Place Order',
 }
 
 function ShippingPage(props) {
+  const userSignin = useSelector((state) => state.userSignin)
+  const { userInfo } = userSignin
+  if (!userInfo) {
+    props.history.push('/signin')
+  }
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddress } = cart
+  const dispatch = useDispatch()
 
-    const userSignin = useSelector((state) => state.userSignin);
-    const { userInfo } = userSignin;
-    if (!userInfo) {
-        props.history.push('/signin');
-    }
-    const cart = useSelector((state) => state.cart);
-    const { shippingAddress } = cart;
-    const dispatch = useDispatch();
+  const [fullName, setFullName] = useState(shippingAddress.fullName)
+  const [address, setAddress] = useState(shippingAddress.address)
+  const [city, setCity] = useState(shippingAddress.city)
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+  const [country, setCountry] = useState(shippingAddress.country)
 
-    const [fullName, setFullName] = useState(shippingAddress.fullName);
-    const [address, setAddress] = useState(shippingAddress.address);
-    const [city, setCity] = useState(shippingAddress.city);
-    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-    const [country, setCountry] = useState(shippingAddress.country);
+  const submitHandler = (e) => {
+    e.preventDefault()
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-
-        dispatch(
-            saveShippingAddress({
-                fullName,
-                address,
-                city,
-                postalCode,
-                country
-            }
-        ))
-        
-        props.history.push('/payment')
-    }
-
-    return (
-        <>
-            <StepsUI step1 step2 text={shippingSteps}></StepsUI>
-            <div className="shipping-container">
-                <form className="shipping-form" onSubmit={submitHandler} >
-                    <div>
-                        <h1>Shipping Address</h1>
-                    </div>
-                    <ul>
-                        <li>
-                            <label htmlFor="fullName">Full Name</label>
-                            <input
-                                type="text"
-                                id="fullName"
-                                placeholder="Enter full name"
-                                value={fullName}
-                                onChange={(e) => setFullName(e.target.value)}
-                                required
-                            ></input>
-                        </li>
-                        <li>
-                            <label htmlFor="address">Address</label>
-                            <input
-                                type="text"
-                                id="address"
-                                placeholder="Enter address"
-                                value={address}
-                                onChange={(e) => setAddress(e.target.value)}
-                                required
-                            ></input>
-                        </li>
-                        <li>
-                            <label htmlFor="city">City</label>
-                            <input
-                                type="text"
-                                id="city"
-                                placeholder="Enter city"
-                                value={city}
-                                onChange={(e) => setCity(e.target.value)}
-                                required
-                            ></input>
-                        </li>
-                        <li>
-                            <label htmlFor="postalCode">Postal Code</label>
-                            <input
-                                type="text"
-                                id="postalCode"
-                                placeholder="Enter postal code"
-                                value={postalCode}
-                                onChange={(e) => setPostalCode(e.target.value)}
-                                required
-                            ></input>
-                        </li>
-                        <li>
-                            <label htmlFor="country">Country</label>
-                            <input
-                                type="text"
-                                id="country"
-                                placeholder="Enter country"
-                                value={country}
-                                onChange={(e) => setCountry(e.target.value)}
-                                required
-                            ></input>
-                        </li>
-                    </ul>
-                    <button className="primary" type="submit">
-                        Continue
-                    </button>
-                </form>
-            </div>
-        </>    
+    dispatch(
+      saveShippingAddress({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
     )
+
+    props.history.push('/payment')
+  }
+
+  return (
+    <>
+      <StepsUI step1 step2 text={shippingSteps}></StepsUI>
+
+      <Box m='2rem 2rem 2rem 2rem'>
+        <Grid container>
+          <Grid item xs={12} sm={3} md={4} lg={4} xl={4} align='center'></Grid>
+          <Grid item xs={12} sm={6} md={4} lg={4} xl={3} align='center'>
+            <Typography gutterBottom variant='h3'>
+              check<span style={{ color: secondary }}>out</span>
+            </Typography>
+            <form onSubmit={submitHandler}>
+              <Typography gutterBottom variant='h6'>
+                {' '}
+                Shipping Address
+              </Typography>
+
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                paddingTop='1rem'
+              >
+                <InputLabel htmlFor='fullName'>Full Name</InputLabel>
+              </Box>
+              <OutlinedInput
+                type='text'
+                name='fullName'
+                id='fullName'
+                required
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+                variant='outlined'
+                margin='dense'
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                paddingTop='2rem'
+              >
+                <InputLabel htmlFor='address'>Street Address</InputLabel>
+              </Box>
+              <OutlinedInput
+                type='text'
+                id='address'
+                name='address'
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                required
+                variant='outlined'
+                margin='dense'
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <Box display='flex'>
+                <Box>
+                  <Box
+                    display='flex'
+                    justifyContent='space-between'
+                    paddingTop='0.5rem'
+                  >
+                    <InputLabel htmlFor='city'>City</InputLabel>
+                  </Box>
+                  <OutlinedInput
+                    type='text'
+                    id='city'
+                    name='city'
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    required
+                    variant='outlined'
+                    margin='dense'
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Box>
+                <Box>
+                  <Box
+                    display='flex'
+                    justifyContent='space-between'
+                    paddingTop='0.5rem'
+                  >
+                    <InputLabel htmlFor='postalCode'>Post Code</InputLabel>
+                  </Box>
+                  <OutlinedInput
+                    type='text'
+                    id='postalCode'
+                    name='postalCode'
+                    value={postalCode}
+                    onChange={(e) => setPostalCode(e.target.value)}
+                    required
+                    variant='outlined'
+                    margin='dense'
+                    fullWidth
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Box>
+              </Box>
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                paddingTop='0.5rem'
+              >
+                <InputLabel htmlFor='country'>Country</InputLabel>
+              </Box>
+              <OutlinedInput
+                type='text'
+                id='country'
+                name='country'
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+                required
+                variant='outlined'
+                margin='dense'
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <Box paddingTop='1rem' display='flex' justifyContent='center'>
+                <Button type='submit' variant='contained' color='primary'>
+                  Continue
+                </Button>
+              </Box>
+            </form>
+          </Grid>
+          <Grid item xs={12} sm={3} md={4} lg={4} xl={4} align='center'></Grid>
+        </Grid>
+      </Box>
+    </>
+  )
 }
 
 export default ShippingPage
